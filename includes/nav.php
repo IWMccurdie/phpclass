a7426315e4f60477b311d054fd68dc6f720088eb
Ian McCurdie <iwmccurdie123@gmail.com>
2025-09-22 19:12:08 -0500

    finished 8Ball

--- includes/nav.php
+++ includes/nav.php
@@ -3,6 +3,7 @@
 $isHome = $_SERVER['REQUEST_URI'] == '/' ? 'selected' : '';
 $isLoops = $_SERVER['REQUEST_URI'] == '/loops/' ? 'selected' : '';
 $isCountdown = $_SERVER['REQUEST_URI'] == '/countdown' ? 'selected' : '';
+$isMagic8Ball = $_SERVER['REQUEST_URI'] == '/magic-8ball' ? 'selected' : '';
 ?>
 
 <nav>
@@ -11,5 +12,6 @@ $isCountdown = $_SERVER['REQUEST_URI'] == '/countdown' ? 'selected' : '';
         <li class="<?=$isHome?>"><a href="/">Home</a></li>
         <li class="<?=$isLoops?>"><a href="/loops">Loops</a></li>
         <li class="<?=$isCountdown?>"><a href="/countdown">Countdown</a></li>
+        <li class="<?=$isMagic8Ball?>"><a href="/magic-8ball">Magic 8 Ball</a></li>
     </ul>
 </nav>
