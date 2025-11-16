5206d022cc632458710226ec0d1b59ff1ebf4479
Ian McCurdie <iwmccurdie123@gmail.com>
2025-11-16 12:05:20 -0600

    Finished Customer Password Hashing Assignment

---
+++ marathon/app/Controllers/Home.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Member;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        helper('form');
+        return view('homepage');
+    }
+
+    public function login(): string
+    {
+
+        echo "here";
+        exit();
+        helper('form');
+
+        $rules = [
+            "username" => "required|valid_email",
+            "password" => "required"
+        ];
+
+        if (!$this->validate($rules))
+        {
+
+            $data = [
+                "load_error" => "true",
+            ];
+            return view('homepage', $data);
+        }
+
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+
+        $Member = new Member();
+        if ($Member->user_login($username, $password))
+        {
+            return view('admin_page');
+        }
+        else {
+            $data = [
+                "load_error" => "true",
+                "error_message" => "Invalid Username or Password."
+            ];
+            return view('homepage', $data);
+        }
+    }
+
+    public function create(): string
+    {
+        echo "create";
+        exit();
+        return view('homepage');
+    }
+
+    public function play($data): string
+    {
+        $data = $data * 12;
+        echo "Hello World -> " . $data;
+        exit();
+    }
+}
--- marathon/app/Views/homepage.php
+++ marathon/app/Views/homepage.php
@@ -59,7 +59,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand topnav" href="#">Start Bootstrap</a>
+            <a class="navbar-brand topnav" href="#">Marathon Master</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -92,20 +92,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="intro-message">
-                    <h1>Landing Page</h1>
-                    <h3>A Template by Start Bootstrap</h3>
-                    <hr class="intro-divider">
-                    <ul class="list-inline intro-social-buttons">
-                        <li>
-                            <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-                        </li>
-                    </ul>
+                    <h1>Marathon Master Page</h1>
+                    <h3>Software that just runs!</h3>
+
                 </div>
             </div>
         </div>
